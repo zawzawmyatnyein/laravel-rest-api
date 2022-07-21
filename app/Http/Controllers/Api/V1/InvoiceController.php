34793232fdda48e9336bafba597889e061032929
Zaw Zaw Myat Nyein <zawzawmyatnyein@gmail.com>
2022-07-21 15:21:28 +0630

    demo completed

---
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Invoice;
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use App\Filters\V1\InvoiceFilter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BulkStoreInvoiceRequest;
+use App\Http\Requests\StoreInvoiceRequest;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Requests\UpdateInvoiceRequest;
+use App\Http\Resources\V1\InvoiceCollection;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $filter = new InvoiceFilter();
+        $filterItems = $filter->transform($request);
+
+        if(count($filterItems) === 0) {
+            return new InvoiceCollection(Invoice::paginate());
+        } else {
+            $invoices = Invoice::where($filterItems)->paginate();
+
+            return new InvoiceCollection($invoices->appends($request->query()));
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreInvoiceRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreInvoiceRequest $request)
+    {
+        //
+    }
+
+    public function bulkStore(BulkStoreInvoiceRequest $request)
+    {
+        $bulk = collect($request->all())->map(function($arr, $key) {
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
+
+        Invoice::insert($bulk->toArray());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invoice $invoice)
+    {
+        return new InvoiceResource($invoice);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateInvoiceRequest  $request
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateInvoiceRequest $request, Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invoice $invoice)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'v1',
+    'namespace' => 'App\Http\Controllers\Api\V1',
+    'middleware' => 'auth:sanctum'
+], function () {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
+
+    Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
+});
+
+
+// customers?postalCode[gt]=30000
